57296a84f7dd6cace321554b13ef5cf2a9d69396
“Jonah <etuafuljonah@gmail.com>
2024-07-03 20:52:51 +0000

    deleted some redundant scripts and streamlined flow of logic in the main execution app

--- wsgi.py
+++ wsgi.py
@@ -4,16 +4,5 @@ from app import create_app
 
 app = create_app(os.getenv("CONFIG_MODE"))
 
-
-# ----------------------------------------------- #
-
-# Hello World!
-@app.route('/')
-def hello():
-    return "Deep Eye!"
-
-
-# ----------------------------------------------- #
-
 if __name__ == "__main__":
     app.run(debug=True)
